73f777aa2ccd24e84270e6534a8e8640e21edf4f
Marwagalal204 <Marwagalal204@gmail.com>
2023-10-17 23:57:43 -0700

    task

--- main.c
+++ main.c
@@ -6,23 +6,12 @@
 */
 int main(void)
 {
-	int check_mode = isatty(STDIN_FILENO), code = 0;
+	int code = 0;
 	char *command;
 
 	while (1)
 	{
-		if (check_mode)
-		{
-			_putchar('#');
-			_putchar('c');
-			_putchar('i');
-			_putchar('s');
-			_putchar('f');
-			_putchar('u');
-			_putchar('n');
-			_putchar('$');
-			_putchar(' ');
-		}
+		_putchar('$');
 
 		command = NULL;
 		readCommand(&command);
